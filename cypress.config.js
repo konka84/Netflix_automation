2e5abbb9f2f9fb37f263da642757616c27fb0531
konka84 <1784konka@gmail.com>
2024-04-15 00:55:36 +0600

    sign up update

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
       // implement node event listeners here
      // baseUrl: 'https://www.netflix.com',
     },
+    testIsolation:false
   },
   
 });
--- cypress/e2e/landingPage.cy.js
+++ cypress/e2e/landingPage.cy.js
@@ -1,34 +1,40 @@
 describe('landingPage', () => {
 
-  // it('verify-title-positive', () => {
-  //   cy.visit('https://www.netflix.com')
-  //   cy.title().should('eq','Netflix Bangladesh - Watch TV Shows Online, Watch Movies Online')
-
-  // })
- /* it('verify-title-negative', () => {
-    cy.visit('https://www.netflix.com')
-    cy.title().should('eq','Unlimited movies, TV shows and more')
+  beforeEach(() => {
+    // Clear cookies
+    cy.clearCookies();
     
+    // Clear local storage
+    cy.clearLocalStorage();
+  });
 
-  })*/
+
+  it('verify the title', () => {
+    cy.visit('https://www.netflix.com')
+    cy.title().should('eq','Netflix Bangladesh - Watch TV Shows Online, Watch Movies Online')
+
+  })
+ 
 it('verify that sign in button exists in landing page',()=>{
  cy.visit('https://www.netflix.com')
  cy.get('a').contains('Sign In').should('be.visible')
 })
-it('verify the functionality  of sign in button for invalid credentials',()=>{
-  cy.visit('https://www.netflix.com')
-  cy.get('a').contains('Sign In').click()
-  cy.wait(2000)
-  cy.get('input[type="email"]').type("konka@gmail.com")
-  cy.get('input[type="password').type("1234")
-  cy.get('button').contains("Sign In").click()
-  cy.wait(6000)
-  //cy.get('div').contains("Sorry, we can't find an account with this email address. Please try again or ").should('exist')
- //cy.get('a').contains('create a new account').click()
-})
+
+// it('verify the functionality  of sign in button for invalid credentials',()=>{
+//   cy.visit('https://www.netflix.com')
+//   cy.get('a').contains('Sign In').click()
+//   cy.wait(2000)
+//   cy.get('input[type="email"]').type("konka@gmail.com")
+//   cy.get('input[type="password').type("1234")
+//   cy.get('button').contains("Sign In").click()
+//   cy.wait(6000)
+//   //cy.get('div').contains("Sorry, we can't find an account with this email address. Please try again or ").should('exist')
+//  //cy.get('a').contains('create a new account').click()
+// })
 it('verify the functionality of sign up',()=>{
+
   cy.visit('https://www.netflix.com')
-  cy.get('input[autocomplete="email"]').eq(0).type("alu123@gmail.com")
+  cy.get('input[autocomplete="email"]').eq(0).type("cat420@yopmail.com")
   cy.wait(6000)
   cy.get('button').contains("Get Started").click()
   cy.get('input#id_password').eq(0).type('doichira123')
@@ -36,23 +42,56 @@ it('verify the functionality of sign up',()=>{
   //cy.get('b').contains("Incorrect password.").should('be.visible')
   cy.get('button').contains('Next').click()
   cy.wait(6000)
-  //cy.get('button').contains('Next').click()
-  //cy.get('class').contains('Premium').should('be.visible')
-  
-  //cy.get('.default-ltr-cache-17piyfc').should('be.visible')
-  //cy.get('class.default-ltr-cache-yi2440').should('be.visible')
- // cy.get('.default-ltr-cache-yi2440').should('be.visible')
-
-  //cy.get('.default-ltr-cache-17piyfc').should('be.visible')
-  cy.get('.form-plan-selection').should('exist')
-  cy.wait(6000)
-  cy.get('.form-plan-selection').find('div.default-ltr-cache-1r8obo9').should('have.length', 4)
-
-  
+  cy.get('button').contains('Next').click()
+  cy.wait(4000)
+  cy.get('div').contains('End-to-end encrypted').should('be.visible')
+  cy.get('div.mopNameAndLogos').click()
+ 
+     
 })
 
+it('Verify card setup functionality with invalid card details',function(){
+  cy.get('label[for="id_creditCardNumber"]').type(4111111111111111)
 
+  cy.wait(2000)
+  cy.get('label[placeholder = "creditExpirationMonth"]').find('input').click({force:true}).type(1227)
+  cy.wait(2000)
+  cy.get('#id_creditCardSecurityCode').type(111)
+  cy.wait(2000)
+  cy.get('#id_firstName').type('Fatema Konka')
+  cy.wait(2000)
+  cy.get('input[name="hasAcceptedTermsOfUse"]').click({force:true})
+  cy.wait(2000)
+  cy.get("button").contains('Start Membership').click({force:true})
+  cy.wait(6000)
+  cy.get('h1.stepTitle').then(($title)=>{
+    let confirmAcc = $title.text()
+    cy.log(confirmAcc)
+  })
+})
 
+it('Verify account confirmation with invalid phone number', function(){
+  cy.get('div.ui-select-current').find('span.country-select-code').should('contain.text','880')
+  cy.wait(3000)
+  cy.get('#id_phoneNumber').click({force:true}).type(12345678)
+  cy.wait(3000)
+  cy.get('button').contains('Text Me').click({force:true})
+  cy.wait(6000)
+  cy.get("div[role='alert']").should('be.visible')
+  cy.get("div[role='alert']").find('span').then(($span)=>{
+    let alert = $span.text()
+    cy.log(alert)
+  })
+})
 
+it('Check signout functionality', function(){
 
+  cy.get('a').contains('Sign Out').click()
+  cy.wait(4000)
+  cy.get('div.logout').should('be.visible')
+  cy.get('a').contains('Go Now').click()
+  cy.wait(8000)
+  cy.get('button').contains('Get Started').should('be.visible')
+
+})
 })
